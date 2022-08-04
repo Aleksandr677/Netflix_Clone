7c8160d2c806ac890329f608434ac3fdf97f0c57
Христиченко Александр <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-08-04 18:57:49 +0300

    Create the project, delete the storyboards

---
+++ Netflix_Clone/Netflix_Clone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone
+//
+//  Created by Христиченко Александр on 2022-08-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
