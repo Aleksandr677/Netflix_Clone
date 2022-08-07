68cba763bc35ee16934f32f746fcc8e5d4a4b78f
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-08-07 14:11:20 +0300

    feat: create custom titleTableViewCell for UpcomingViewController

---
+++ Netflix_Clone/Netflix_Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix_Clone
+//
+//  Created by Христиченко Александр on 2022-08-07.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
