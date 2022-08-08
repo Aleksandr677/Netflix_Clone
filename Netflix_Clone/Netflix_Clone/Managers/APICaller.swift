992e6f229dbf7eb86fdca3f846fcefecb8f7b684
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-08-08 21:31:28 +0300

    feat: work on searchViewController

--- Netflix_Clone/Netflix_Clone/Managers/APICaller.swift
+++ Netflix_Clone/Netflix_Clone/Managers/APICaller.swift
@@ -10,6 +10,7 @@ import Foundation
 struct Constants {
     static let API_KEY = "f55db091e690d1954e6812b3fa9656ed"
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = "AIzaSyAX8Ym1Byo79F1H5kmYjoYinL9CLPQMIAw"
 }
 
 enum APIError: Error {
@@ -95,6 +96,23 @@ class APICaller {
     
     func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else { return }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        task.resume()
+    }
+    
+    func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void) {
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        guard let url = URL(string: "\(Constants.baseURL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else { return }
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else { return }
             
